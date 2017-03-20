9bc5f4c7765944f24e4662b6a441fd17777f44ba
vintosarails <vintosa@gmail.com>
2017-03-20 15:22:44 +0300

    complete edit/

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,7 @@
 class PinsController < ApplicationController
 	before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+  	before_action :signed_in_user, only: [:edit, :update]
+  	before_action :correct_user,   only: [:edit, :update]
 	def index
 		@pins = Pin.all.order("created_at DESC")
 	end
@@ -49,6 +51,14 @@ class PinsController < ApplicationController
 		params.require(:pin).permit(:title, :description, :image)
 	end
 
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 
 	def find_pin
 		@pin = Pin.find(params[:id])
