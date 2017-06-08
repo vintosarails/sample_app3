35eee20b0c613c9da9bd2c349fc105eea690df11
vintosarails <vintosa@gmail.com>
2017-06-08 23:02:53 +0300

    cloud

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -6,7 +6,10 @@ class Pin < ActiveRecord::Base
 
 	has_many :comments
 
-	has_attached_file :image, styles: {  }
+	#has_attached_file :image, styles: {  }
+	has_attached_file :image,
+  	:storage => :cloudinary,
+  	:cloudinary_credentials => Rails.root.join("config/cloudinary.yml")
  	#has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   	
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
